b11e013cd54ee31c9feb7cc14a16cf4b0cdb7d84
Nat-Serrano <naticio@gmail.com>
2021-04-27 11:52:55 -0500

    Initial Commit

---
+++ Karaoking/KaraokingApp.swift
@@ -0,0 +1,17 @@
+//
+//  KaraokingApp.swift
+//  Karaoking
+//
+//  Created by Nat-Serrano on 4/27/21.
+//
+
+import SwiftUI
+
+@main
+struct KaraokingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
